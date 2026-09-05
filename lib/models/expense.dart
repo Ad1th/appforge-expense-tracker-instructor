d2f08f8865eee450a5b259a331401e2c57c15935
ad1th <adith2505@outlook.com>
2026-09-05 22:34:15 +0530

    feat: add a date to the Expense model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,12 +1,15 @@
 /// A single expense the user has recorded.
 ///
-/// Every expense has a short [title] and an [amount] of money.
+/// Every expense has a short [title], an [amount] of money and the
+/// [date] it happened.
 class Expense {
   Expense({
     required this.title,
     required this.amount,
+    required this.date,
   });
 
   final String title;
   final double amount;
+  final DateTime date;
 }
